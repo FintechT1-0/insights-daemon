52c7de715c5b7f08c2179e07c1bd7f9cb09bbf7f
Lyubomyr <lsokhat06@gmail.com>
2025-11-19 18:35:43 +0200

    Improved logic of saving news

--- daemon/utils.py
+++ daemon/utils.py
@@ -191,15 +191,16 @@ def validate_items(raw_items: list[dict]) -> list[NewsItem]:
 async def collect_news():
     logger.debug(f"Trying to collect news at {datetime.utcnow()}")
 
-    await delete_all_articles()
-
     validated_EN = validate_items(get_daily_news_EN())
     validated_UA = validate_items(get_daily_news_UA())
 
-    await save_news_items(validated_EN + validated_UA)
-
-    gc.collect()
-
+    if len(validated_EN) < 5 or len(validated_UA) < 5:
+        logger.debug(f"Not enough news to renew: {len(validated_EN)} for EN and {len(validated_UA)} for UA")
     
+    else:
+        await delete_all_articles()
+        await save_news_items(validated_EN + validated_UA)
+    
+    gc.collect()
 
 
